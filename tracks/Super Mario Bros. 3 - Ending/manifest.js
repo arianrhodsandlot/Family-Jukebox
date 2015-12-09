0f3e87142654cb886055ef971d0b1329102dcc88
Arianrhod <theguidanceofawhitetower@gmail.com>
2015-12-10 01:17:31 +0800

    start a new track

---
+++ tracks/Super Mario Bros. 3 - Ending/manifest.js
@@ -0,0 +1,11 @@
+define(
+  ['channels/square1.js', 'channels/square2.js', 'channels/triangle.js', 'channels/noise.js'],
+  function (square1, square2, triangle, noise) {
+    return {
+      game: 'Super Mario Bros. 3',
+      title: 'Ending',
+      year: '1988',
+      company: 'Nintendo',
+      channels: [square2]
+    }
+})
