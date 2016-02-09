09b86377066ca2c39cba59f66eb2850939b31e50
arianrhod <theguidanceofawhitetower@gmail.com>
2016-02-09 16:53:03 +0800

    add The Legend of Zelda - Outworld

---
+++ tracks/The Legend of Zelda - Outworld/channels/square2.js
@@ -0,0 +1,41 @@
+define(['_'], function (_) {
+  var prelude = [
+    -9, -7, -5, -4, -2, 0, 2
+  ]
+
+  var overtune = [
+    -9, -7, -5, -4, -2, 0, 2
+  ]
+
+  var verse = [
+    -9, -7, -5, -4, -2, 0, 2
+  ]
+
+  var chorus = [
+    -9, -7, -5, -4, -2, 0, 2
+  ]
+
+  var notes = prelude
+    .concat(overtune)
+    .concat(verse)
+    .concat(chorus)
+
+  return {
+    name: 'square2',
+    waveform: 'square',
+    config: {
+      instrument: {
+        sampleRate: 44100,
+        bpm: 300,
+        fadeOut: {
+          from: 0.8,
+          to: 1
+        }
+      },
+      audio: {
+        volume: 0.2
+      }
+    },
+    notes: notes
+  }
+})
---
+++ tracks/The Legend of Zelda - Outworld/manifest.js
@@ -0,0 +1,11 @@
+define(
+  ['channels/square1.js', 'channels/square2.js', 'channels/triangle.js', 'channels/noise.js'],
+  function (square1, square2, triangle, noise) {
+    return {
+      game: 'The Legend of Zelda',
+      title: 'Outworld',
+      year: '1986',
+      company: 'Nintendo',
+      channels: [square1, square2, triangle, noise]
+    }
+  })
