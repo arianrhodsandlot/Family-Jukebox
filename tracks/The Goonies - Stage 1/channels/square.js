57e82ed9e89f3958b835a611110e24eb7d496ab7
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2016-05-02 17:40:48 +0800

    add square prelude

--- tracks/The Goonies - Stage 1/channels/square.js
+++ tracks/The Goonies - Stage 1/channels/square.js
@@ -1,6 +1,21 @@
 define(['_'], function (_) {
   var prelude = [
-    -7, 0, 5, -7, -2, 0, 4, 5
+    [-7, 0.5], [null, 1.5],
+    [5, 0.5], [null, 0.5], [-7, 0.5], [null, 0.5],
+    [-2, 0.5], [null, 0.5], [0, 0.5], [null, 0.5], [3, 0.5], [null, 0.5], [5, 0.5], [null, 1.5],
+
+    [-7, 0.5], [-7, 0.5],
+    [5, 0.5], [null, 0.5], [-7, 0.5], [null, 0.5],
+    [-2, 0.5], [null, 0.5], [0, 0.5], [null, 0.5], [3, 0.5], [null, 0.5], [5, 0.5], [null, 0.5],
+
+    [-7, 0.5], [null, 1.5],
+    [5, 0.5], [null, 0.5], [-7, 0.5], [null, 0.5],
+    [-2, 0.5], [null, 0.5], [0, 0.5], [null, 0.5], [3, 0.5], [null, 0.5], [5, 0.5], [null, 1.5],
+
+    -7, 5, -7, -2, 0, 10, 8, [12, 3],
+    [14, 0.5], [16, 0.5], [17, 12],
+
+    [9, 3], [7, 3], [9, 10]
   ]
 
   var overtune = [
@@ -10,9 +25,10 @@ define(['_'], function (_) {
   ]
 
   var chorus = [
+    [null, 100]
   ]
 
-  var notes = prelude
+  var notes = verse.concat(chorus);prelude
     .concat(overtune)
     .concat(verse)
     .concat(chorus)
@@ -25,8 +41,8 @@ define(['_'], function (_) {
         sampleRate: 44100,
         bpm: 300,
         fadeOut: {
-          from: 0.8,
-          to: 1
+          from: 0,
+          to: 2.5
         }
       },
       audio: {
